79cce2994135a0d004d7837fca95408c58f684a6
RodH257 <rodhowarth257@gmail.com>
2011-05-06 19:48:34 +1000

    Initial version - checks for any packet on any interface on port 808

---
+++ RevitServerMonitor/RevitServerMonitorForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace RevitServerMonitor
+{
+    partial class RevitServerMonitorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RevitServerMonitorForm));
+            this.lblCommunicating = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCommunicating
+            // 
+            this.lblCommunicating.AutoSize = true;
+            this.lblCommunicating.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCommunicating.ForeColor = System.Drawing.Color.Green;
+            this.lblCommunicating.Location = new System.Drawing.Point(45, 96);
+            this.lblCommunicating.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCommunicating.Name = "lblCommunicating";
+            this.lblCommunicating.Size = new System.Drawing.Size(626, 37);
+            this.lblCommunicating.TabIndex = 0;
+            this.lblCommunicating.Text = "Not communicating. Safe to disconnect.";
+            this.lblCommunicating.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Local Server Activity Status";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(548, 197);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(156, 34);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(12, 197);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(156, 34);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "Minmize to Tray";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Rods Revit Server Monitor";
+            this.trayIcon.Visible = true;
+            this.trayIcon.Click += new System.EventHandler(this.trayIcon_Click);
+            // 
+            // RevitServerMonitorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 243);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCommunicating);
+            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "RevitServerMonitorForm";
+            this.Text = "Rod Howarth\'s Revit Server Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.RevitServerMonitorForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCommunicating;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+    }
+}
+
---
+++ RevitServerMonitor/RevitServerMonitorForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PacketDotNet;
+using RevitServerMonitor.Properties;
+using SharpPcap;
+
+namespace RevitServerMonitor
+{
+    public partial class RevitServerMonitorForm : Form
+    {
+        //delgate for updating ui
+        private delegate void UpdateUiHandler(bool communicating);
+        //timer for turning off the communication
+        private Timer _endCommunicatingTimer = new Timer();
+
+        public RevitServerMonitorForm()
+        {
+            InitializeComponent();
+
+            //setup timer
+            _endCommunicatingTimer.Interval = 5 * 1000;
+            _endCommunicatingTimer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //start capturing for all interfaces
+            foreach (ICaptureDevice device in CaptureDeviceList.Instance)
+            {
+                    device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+                    device.Open(DeviceMode.Normal, 1000);
+                    device.StartCapture();
+            }
+        }
+
+      
+        //check the packet is port 808
+        void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            if (e.Packet.Data.Length < 20)
+                return;
+
+            Packet p = null;
+            TcpPacket tcp = null;
+            try
+            {
+                p = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+                tcp = TcpPacket.GetEncapsulated(p);
+
+            }
+            catch
+            {
+                return;
+            }
+            if (tcp == null)
+                return;
+            ushort portToFind = 808;
+            if (tcp.DestinationPort == portToFind || tcp.DestinationPort == portToFind)
+            {
+                UpdateUiHandler handler = new UpdateUiHandler(SetCommunicating);
+                Object[] args = new Object[] { true };
+                this.BeginInvoke(handler, args);
+            }
+
+
+        }
+
+      
+        /// <summary>
+        /// Sets communicating display on or off
+        /// </summary>
+        void SetCommunicating(bool communicating)
+        {
+            if (communicating)
+            {
+
+                this.lblCommunicating.Text = "COMMUNICATING - DO NOT DISCONNECT";
+                this.lblCommunicating.ForeColor = Color.Red;
+                this.trayIcon.Icon = Resources.RedArrow;
+                //set an event for in 5 seconds time to swap it to not communicating.
+                _endCommunicatingTimer.Stop();
+                _endCommunicatingTimer.Start();
+            }
+            else
+            {
+                this.lblCommunicating.ForeColor = Color.Green;
+                this.trayIcon.Icon = Resources.GreenArrow;
+                this.lblCommunicating.Text = "Not communicating. Safe to disconnect";
+                _endCommunicatingTimer.Stop();
+            }
+        }
+
+
+        /// <summary>
+        /// Sets it to not communicating
+        /// </summary>
+        void timer_Tick(object sender, EventArgs e)
+        {
+            SetCommunicating(false);
+        }
+
+        /// <summary>
+        /// Stop Capturing on Close
+        /// </summary>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (ICaptureDevice device in CaptureDeviceList.Instance)
+            {
+                device.StopCapture();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        
+        private void trayIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void RevitServerMonitorForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                Hide();
+        }
+    }
+}
